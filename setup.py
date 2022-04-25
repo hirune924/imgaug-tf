71c7eb09ff2e90c16cf41379912407a5d7c45904
hirune924 <36130517+hirune924@users.noreply.github.com>
2022-04-25 21:46:48 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='imgaugtf',
     version='0.1',
     packages=find_packages()
+    url="https://github.com/hirune924/imgaug-tf/",
     package_dir={"": "src"},
-    py_modules=["imgaugtf"],
+    py_modules=["imgaugtf"]
 )
