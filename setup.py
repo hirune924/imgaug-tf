e690ce969eadff7ae32065dc6bf720fdaa0ded67
hirune924 <36130517+hirune924@users.noreply.github.com>
2022-04-26 09:03:47 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='imgaugtf',
     version='0.1',
-    #url="https://github.com/hirune924/imgaug-tf/",
+    author="hirune924",
+    description="tensorflow native image augmantation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hirune924/imgaug-tf/",
     packages=find_packages(),
-    #package_dir={"imgaugtf": "src"},
-    #py_modules=["functions", 'augmentations', 'batch_augmentations', 'utils'],
-    #package_dir={"": "src"},
+    license="Apache License Version 2.0",
+    install_requires=[
+        "tensorflow >= 2.0",
+        "tensorflow-addons >= 0.7.1",
+        ],
+    python_requires='>=3.4',
 )
