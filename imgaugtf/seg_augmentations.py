0d9ad9d172d704fec65cf5758b07b0bcb1a6a714
hirune924 <36130517+hirune924@users.noreply.github.com>
2022-04-26 18:42:46 +0900

    add segmentation module

--- imgaugtf/seg_augmentations.py
+++ imgaugtf/seg_augmentations.py
@@ -8,7 +8,7 @@ __all__ = [
     'random_flip_left_right',
 ]
 
-def apply_func_with_prob(func, image, mask, args, prob):
+def apply_func_with_prob_mask(func, image, mask, args, prob):
     """Apply `func` to image w/ `args` as input with probability `prob`."""
 
     # Apply the function with probability `prob`.
@@ -25,5 +25,33 @@ def apply_func_with_prob(func, image, mask, args, prob):
         lambda: mask)
     return augmented_image, augmented_mask
 
+def apply_func_with_prob(func, image, args, prob):
+    """Apply `func` to image w/ `args` as input with probability `prob`."""
+
+    # Apply the function with probability `prob`.
+    should_apply_op = tf.cast(
+        tf.floor(tf.random.uniform([], dtype=tf.float32) + prob), tf.bool)
+    augmented_image = tf.cond(
+        should_apply_op,
+        lambda: func(image, *args),
+        lambda: image)
+    return augmented_image
+
 def random_flip_left_right(image, mask, prob=0.5):
-    return apply_func_with_prob(tf.image.flip_left_right, image, mask, (), prob)
+    return apply_func_with_prob_mask(tf.image.flip_left_right, image, mask, (), prob)
+
+def random_flip_up_down(image, mask, prob=0.5):
+    return apply_func_with_prob_mask(tf.image.flip_up_down, image, mask, (), prob)
+
+def random_solarize(image, mask, threshold=128, prob=0.5):
+    return apply_func_with_prob(F.solarize, image, (threshold, ), prob), mask
+
+def random_solarize_add(image, mask, addition=30, threshold=128, prob=0.5):
+
+    return apply_func_with_prob(F.solarize_add, image, (addition, threshold, ), prob), mask
+
+def random_color(image, mask, alpha_range=(0.2, 0.8), prob=0.5):
+    '''if alpha is 0, return gray image. alpha is 1, do nothing.'''
+    alpha = tf.random.uniform([], minval=alpha_range[0], maxval=alpha_range[1], dtype=tf.float32)
+    return apply_func_with_prob(F.color, image, (alpha, ), prob), mask
+    
