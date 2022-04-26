da072a0ad5f9e9779e84eb7b6724c7a0ce5ddd52
hirune924 <36130517+hirune924@users.noreply.github.com>
2022-04-26 13:57:14 +0900

    test

--- imgaugtf/__init__.py
+++ imgaugtf/__init__.py
@@ -1,5 +1,5 @@
-from .augmentations import *
-#from . import augmentations as aug
+#from .augmentations import *
+from . import augmentations as aug
 from .utils import *
 from .functional import *
 from .batch_augmentations import *
