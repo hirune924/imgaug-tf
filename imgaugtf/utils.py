7ec4954084c55b7b91a74686f350d08e5b7cb5d9
hirune924 <36130517+hirune924@users.noreply.github.com>
2022-04-26 14:14:23 +0900

    test

--- imgaugtf/utils.py
+++ imgaugtf/utils.py
@@ -3,8 +3,8 @@ import tensorflow_addons as tfa
 import tensorflow_probability as tfp
 import math
 
-from imgaugtf.augmentations import *
-
+#from imgaugtf.augmentations import *
+from . import augmentations as aug
 __all__ = [
     'apply_one',
     'apply_n',
@@ -14,54 +14,54 @@ __all__ = [
 ]
 
 OPERATORS = [
-    {'func':random_flip_left_right, 'option':{}},
-    {'func':random_flip_up_down, 'option':{}},
-    {'func':random_cutout, 'option':{'num_holes': 8, 'hole_size': 20, 'replace': 0}},
-    {'func':random_solarize, 'option':{'threshold': 128}},
-    {'func':random_solarize_add, 'option':{'addition':30, 'threshold': 128}},
-    {'func':random_color, 'option':{'alpha_range': (0.2, 0.8)}},
-    {'func':random_contrast, 'option':{'lower': 0.2, 'upper': 0.8}},
-    {'func':random_brightness, 'option':{'max_delta': 0.1}},
-    {'func':random_posterize, 'option':{'bits': 4}},
-    {'func':random_rotate, 'option':{'degree_range': (-90, 90), 'replace': 0}},
-    {'func':random_invert, 'option':{}},
-    {'func':random_equalize, 'option':{'bins': 256}},
-    {'func':random_sharpness, 'option':{'alpha_range': (-3.0, 3.0)}},
-    {'func':random_autocontrast, 'option':{}},
-    {'func':random_translate_x, 'option':{'percent': 0.5, 'replace': 0}},
-    {'func':random_translate_y, 'option':{'percent': 0.5, 'replace': 0}},
-    {'func':random_shear_x, 'option':{'percent': 0.3, 'replace': 0}},
-    {'func':random_shear_y, 'option':{'percent': 0.3, 'replace': 0}},
-    {'func':random_hsv_in_yiq, 'option':{'max_delta_hue': 0.2, 'lower_saturation': 0.5, 'upper_saturation': 1.0, 'lower_value': 0.5, 'upper_value': 1.0}},
-    {'func':random_gaussian_filter2d, 'option':{'filter_shape_range': (3,7)}},
-    {'func':random_mean_filter2d, 'option':{'filter_shape': (3,3)}},
-    {'func':random_median_filter2d, 'option':{'filter_shape': (3,3)}}
+    {'func':aug.random_flip_left_right, 'option':{}},
+    {'func':aug.random_flip_up_down, 'option':{}},
+    {'func':aug.random_cutout, 'option':{'num_holes': 8, 'hole_size': 20, 'replace': 0}},
+    {'func':aug.random_solarize, 'option':{'threshold': 128}},
+    {'func':aug.random_solarize_add, 'option':{'addition':30, 'threshold': 128}},
+    {'func':aug.random_color, 'option':{'alpha_range': (0.2, 0.8)}},
+    {'func':aug.random_contrast, 'option':{'lower': 0.2, 'upper': 0.8}},
+    {'func':aug.random_brightness, 'option':{'max_delta': 0.1}},
+    {'func':aug.random_posterize, 'option':{'bits': 4}},
+    {'func':aug.random_rotate, 'option':{'degree_range': (-90, 90), 'replace': 0}},
+    {'func':aug.random_invert, 'option':{}},
+    {'func':aug.random_equalize, 'option':{'bins': 256}},
+    {'func':aug.random_sharpness, 'option':{'alpha_range': (-3.0, 3.0)}},
+    {'func':aug.random_autocontrast, 'option':{}},
+    {'func':aug.random_translate_x, 'option':{'percent': 0.5, 'replace': 0}},
+    {'func':aug.random_translate_y, 'option':{'percent': 0.5, 'replace': 0}},
+    {'func':aug.random_shear_x, 'option':{'percent': 0.3, 'replace': 0}},
+    {'func':aug.random_shear_y, 'option':{'percent': 0.3, 'replace': 0}},
+    {'func':aug.random_hsv_in_yiq, 'option':{'max_delta_hue': 0.2, 'lower_saturation': 0.5, 'upper_saturation': 1.0, 'lower_value': 0.5, 'upper_value': 1.0}},
+    {'func':aug.random_gaussian_filter2d, 'option':{'filter_shape_range': (3,7)}},
+    {'func':aug.random_mean_filter2d, 'option':{'filter_shape': (3,3)}},
+    {'func':aug.random_median_filter2d, 'option':{'filter_shape': (3,3)}}
 ]
 PIXEL_OPERATORS = [
-    {'func':random_cutout, 'option':{'num_holes': 8, 'hole_size': 20, 'replace': 0}},
-    {'func':random_solarize, 'option':{'threshold': 128}},
-    {'func':random_solarize_add, 'option':{'addition':30, 'threshold': 128}},
-    {'func':random_color, 'option':{'alpha_range': (0.2, 0.8)}},
-    {'func':random_contrast, 'option':{'lower': 0.2, 'upper': 0.8}},
-    {'func':random_brightness, 'option':{'max_delta': 0.1}},
-    {'func':random_posterize, 'option':{'bits': 4}},
-    {'func':random_invert, 'option':{}},
-    {'func':random_equalize, 'option':{'bins': 256}},
-    {'func':random_sharpness, 'option':{'alpha_range': (-3.0, 3.0)}},
-    {'func':random_autocontrast, 'option':{}},
-    {'func':random_hsv_in_yiq, 'option':{'max_delta_hue': 0.2, 'lower_saturation': 0.5, 'upper_saturation': 1.0, 'lower_value': 0.5, 'upper_value': 1.0}},
-    {'func':random_gaussian_filter2d, 'option':{'filter_shape_range': (3,7)}},
-    {'func':random_mean_filter2d, 'option':{'filter_shape': (3,3)}},
-    {'func':random_median_filter2d, 'option':{'filter_shape': (3,3)}}
+    {'func':aug.random_cutout, 'option':{'num_holes': 8, 'hole_size': 20, 'replace': 0}},
+    {'func':aug.random_solarize, 'option':{'threshold': 128}},
+    {'func':aug.random_solarize_add, 'option':{'addition':30, 'threshold': 128}},
+    {'func':aug.random_color, 'option':{'alpha_range': (0.2, 0.8)}},
+    {'func':aug.random_contrast, 'option':{'lower': 0.2, 'upper': 0.8}},
+    {'func':aug.random_brightness, 'option':{'max_delta': 0.1}},
+    {'func':aug.random_posterize, 'option':{'bits': 4}},
+    {'func':aug.random_invert, 'option':{}},
+    {'func':aug.random_equalize, 'option':{'bins': 256}},
+    {'func':aug.random_sharpness, 'option':{'alpha_range': (-3.0, 3.0)}},
+    {'func':aug.random_autocontrast, 'option':{}},
+    {'func':aug.random_hsv_in_yiq, 'option':{'max_delta_hue': 0.2, 'lower_saturation': 0.5, 'upper_saturation': 1.0, 'lower_value': 0.5, 'upper_value': 1.0}},
+    {'func':aug.random_gaussian_filter2d, 'option':{'filter_shape_range': (3,7)}},
+    {'func':aug.random_mean_filter2d, 'option':{'filter_shape': (3,3)}},
+    {'func':aug.random_median_filter2d, 'option':{'filter_shape': (3,3)}}
 ]
 GEO_OPERATORS = [
-    {'func':random_flip_left_right, 'option':{}},
-    {'func':random_flip_up_down, 'option':{}},
-    {'func':random_rotate, 'option':{'degree_range': (-90, 90), 'replace': 0}},
-    {'func':random_translate_x, 'option':{'percent': 0.5, 'replace': 0}},
-    {'func':random_translate_y, 'option':{'percent': 0.5, 'replace': 0}},
-    {'func':random_shear_x, 'option':{'percent': 0.3, 'replace': 0}},
-    {'func':random_shear_y, 'option':{'percent': 0.3, 'replace': 0}},
+    {'func':aug.random_flip_left_right, 'option':{}},
+    {'func':aug.random_flip_up_down, 'option':{}},
+    {'func':aug.random_rotate, 'option':{'degree_range': (-90, 90), 'replace': 0}},
+    {'func':aug.random_translate_x, 'option':{'percent': 0.5, 'replace': 0}},
+    {'func':aug.random_translate_y, 'option':{'percent': 0.5, 'replace': 0}},
+    {'func':aug.random_shear_x, 'option':{'percent': 0.3, 'replace': 0}},
+    {'func':aug.random_shear_y, 'option':{'percent': 0.3, 'replace': 0}},
 ]
 
 def apply_one(image, functions=OPERATORS, prob=1.0):
